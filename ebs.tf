23abc8aa7167f4a227ae309a9f9fc3fcfb8ab1e4
Roee Segev <emaohi@gmail.com>
2025-05-04 15:24:46 +0300

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -1,4 +1,10 @@
 resource "aws_ebs_volume" "v1" {
+  new_attr = 1
   availability_zone = "us-west-2a"
-  size = 40
-} 
+  size = 100
+}
+
+resource "aws_ebs_volume" "v2" {
+  availability_zone = "us-west-2a"
+  size = 100
+}
